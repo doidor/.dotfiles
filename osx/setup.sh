297d34ff9842cb57530d43ac8e2f47592afd3b8e
Tudor Popa <tdr.popa@gmail.com>
2021-12-13 09:05:48 +0200

    added dependencies as comments in setup file

--- osx/setup.sh
+++ osx/setup.sh
@@ -3,3 +3,15 @@
 echo -e "Copying dotfiles..."
 rsync -avr --exclude='setup.sh' ./ ~/ 
 echo -e "Done."
+
+# echo -e "Installing dependencies..."
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# brew update
+# brew install --cask visual-studio-code
+# brew install tmux
+# curl -L https://nixos.org/nix/install | sh
+# nix-env -i neovim direnv 
+# https://rectangleapp.com/
+# https://alt-tab-macos.netlify.app/
+# https://github.com/leits/MeetingBar
+# https://github.com/alacritty/alacritty/releases

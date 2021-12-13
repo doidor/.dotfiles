23c719191789fbb806d6c31659f89eaa7ae9f040
Tudor Popa <tdr.popa@gmail.com>
2021-12-13 12:44:43 +0200

    added nvim plugins

--- osx/setup.sh
+++ osx/setup.sh
@@ -11,6 +11,10 @@ echo -e "Done."
 # brew install tmux
 # curl -L https://nixos.org/nix/install | sh
 # nix-env -i neovim direnv 
+
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 # https://rectangleapp.com/
 # https://alt-tab-macos.netlify.app/
 # https://github.com/leits/MeetingBar

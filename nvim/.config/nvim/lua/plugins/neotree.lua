7d4674b425e325345a8daef9a6e1ddc6ac881255
Tudor Popa <tdr.popa@gmail.com>
2024-04-14 17:31:52 +0200

    use oil instead of neotree

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,43 +1,44 @@
-return {
-  "nvim-neo-tree/neo-tree.nvim",
+return {}
+-- return {
+--   "nvim-neo-tree/neo-tree.nvim",
 
-  lazy = false,
+--   lazy = false,
 
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+--     "MunifTanjim/nui.nvim",
+--   },
 
-  config = function ()
-    require('neo-tree').setup({
-      close_if_last_window = true,
+--   config = function ()
+--     require('neo-tree').setup({
+--       close_if_last_window = true,
 
-      window = {
-        position = "current",
-        mappings = {
-          ["<cr>"] = "open",
-          ["l"] = "open",
-          ["h"] = "close_node",
-        },
-      },
+--       window = {
+--         position = "current",
+--         mappings = {
+--           ["<cr>"] = "open",
+--           ["l"] = "open",
+--           ["h"] = "close_node",
+--         },
+--       },
 
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = true,
-        },
+--       filesystem = {
+--         filtered_items = {
+--           visible = true,
+--           hide_dotfiles = false,
+--           hide_gitignored = true,
+--         },
 
-        follow_current_file = {
-          enabled = true,
-          leave_dirs_open = true,
-        },
+--         follow_current_file = {
+--           enabled = true,
+--           leave_dirs_open = true,
+--         },
 
-        hijack_netrw_behavior = "open_default"
-      },
-    })
+--         hijack_netrw_behavior = "open_default"
+--       },
+--     })
 
-    vim.keymap.set("n", "-", ":Neotree reveal<CR>")
-  end
-}
+--     vim.keymap.set("n", "-", ":Neotree reveal<CR>")
+-- --   end
+-- }

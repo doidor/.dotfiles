6069a92b2446548e15a432902700d78f72bf66e5
Tudor Popa <tdr.popa@gmail.com>
2024-04-15 09:44:20 +0200

    update oil config

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -11,9 +11,12 @@ return {
       view_options = {
         show_hidden = true
       },
+
       keymaps = {
         ['<C-p>'] = false,
-      }
+      },
+
+      skip_confirm_for_simple_edits = true
     })
 
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })

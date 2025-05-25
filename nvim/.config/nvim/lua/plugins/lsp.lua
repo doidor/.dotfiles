a29e964ca76f4295e8b255af931f8586902cad91
Tudor Popa <tdr.popa@gmail.com>
2025-05-25 14:32:31 +0200

    add pyrefly as default python type checker

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -30,7 +30,7 @@ return {
 
       require('mason').setup({})
       require('mason-lspconfig').setup({
-        ensure_installed = { 'ts_ls', 'rust_analyzer', 'astro', 'bashls', 'cssls', 'jsonls', 'lua_ls', 'pylsp' },
+        ensure_installed = { 'ts_ls', 'rust_analyzer', 'astro', 'bashls', 'cssls', 'jsonls', 'lua_ls', 'pylsp', 'pyrefly' },
         handlers = {
           lsp_zero.default_setup,
           lua_ls = function()
